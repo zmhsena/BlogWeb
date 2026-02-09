7c45c3524917beebd1321712e5f860497790772d
zmhsena <2482811513@qq.com>
2026-02-09 19:54:02 +0800

    Admin Delete/Update: 2026/2/9 19:53:31

--- data.js
+++ data.js
@@ -5,12 +5,5 @@ window.blogPosts = [
     "excerpt": "上下文```using System;namespace BlogWeb{    public class ...",
     "fullContent": "上下文\n```\nusing System;\n\nnamespace BlogWeb\n{\n    public class Program\n    {\n        private static void Main(string[] args)\n        {\n            Console.Write(\"这是个测试\");\n        }\n    }\n}\n```",
     "category": "测试"
-  },
-  {
-    "title": "正式测试标题",
-    "date": "2026-02-09",
-    "excerpt": "测试内容` ` `public void SayHello() {    Console.WriteLine(&q...",
-    "fullContent": "测试内容\n` ` `\npublic void SayHello() {\n    Console.WriteLine(&quot;Hello World&quot;);\n}\n` ` `",
-    "category": "测试"
   }
 ];
