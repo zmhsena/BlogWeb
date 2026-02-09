fb844cb07166ef743db22371c0af703e4dc292da
zmhsena <2482811513@qq.com>
2026-02-09 17:40:59 +0800

    Admin Delete/Update: 2026/2/9 17:40:28

--- data.js
+++ data.js
@@ -2,8 +2,8 @@ window.blogPosts = [
   {
     "title": "正式测试标题",
     "date": "2026-02-09",
-    "excerpt": "测试内容...",
-    "fullContent": "测试内容",
+    "excerpt": "测试内容` ` `c#public void SayHello() {    Console.WriteLine(...",
+    "fullContent": "测试内容\n` ` `c#\npublic void SayHello() {\n    Console.WriteLine(&quot;Hello World&quot;);\n}\n` ` `",
     "category": "测试"
   }
 ];
