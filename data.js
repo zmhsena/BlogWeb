9ec319e7345f8cc514f4a5381bc7472aa212377d
zmhsena <2482811513@qq.com>
2026-02-09 20:16:17 +0800

    Admin Delete/Update: 2026/2/9 20:15:47

--- data.js
+++ data.js
@@ -1,5 +1,12 @@
 window.blogPosts = [
   {
+    "title": "1",
+    "date": "2026-02-09",
+    "excerpt": "1...",
+    "fullContent": "1",
+    "category": "1"
+  },
+  {
     "title": "代码块测试",
     "date": "2026-02-09",
     "excerpt": "上下文```using System;namespace BlogWeb{    public class ...",
