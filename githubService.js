09c9202cbe4b23d7df817ba23379d2258a037e27
hzongy <hzongy@wooduan.com>
2026-02-09 15:48:15 +0800

    在admin添加token输入栏

--- githubService.js
+++ githubService.js
@@ -1,21 +1,11 @@
-// githubService.js 里面不要放真正的 Token 字符串
+// githubService.js
 const GITHUB_CONFIG = {
     repo: "zmhsena/BlogWeb",
     path: "data.js",
-    branch: "master" // 你的分支是 master
+    branch: "master"
 };
 
-async function updateGitHubData(newContent) {
-    // 优先从本地存储获取，如果没有则弹窗询问
-    let token = localStorage.getItem('my_gh_token');
-
-    if (!token) {
-        token = prompt("请输入您的 GitHub Personal Access Token:");
-        if (token) localStorage.setItem('my_gh_token', token);
-    }
-
-    if (!token) return alert("没有 Token 无法保存");
-
+async function updateGitHubData(newContent, token) { // 接收 token 参数
     const url = `https://api.github.com/repos/${GITHUB_CONFIG.repo}/contents/${GITHUB_CONFIG.path}`;
 
     try {
@@ -32,7 +22,7 @@ async function updateGitHubData(newContent) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                message: `Update blog: ${new Date().toLocaleString()}`,
+                message: `Admin update: ${new Date().toLocaleString()}`,
                 content: btoa(unescape(encodeURIComponent(newContent))),
                 sha: sha,
                 branch: GITHUB_CONFIG.branch
