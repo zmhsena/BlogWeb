5da32b5e02bd26dd599f67b3dcf710e067fdd81d
zmhsena <2482811513@qq.com>
2026-02-10 13:39:34 +0800

    Admin update: 2026/2/10 13:39:03

--- data.js
+++ data.js
@@ -1,9 +1,1 @@
-window.blogPosts = [
-  {
-    "title": "代码块测试",
-    "date": "2026-02-09",
-    "excerpt": "上下文```using System;namespace BlogWeb{    public class ...",
-    "fullContent": "上下文\n```\nusing System;\n\nnamespace BlogWeb\n{\n    public class Program\n    {\n        private static void Main(string[] args)\n        {\n            Console.Write(\"这是个测试\");\n        }\n    }\n}\n```",
-    "category": "测试"
-  }
-];
+window.blogPosts = [];
