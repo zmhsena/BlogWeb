a580eef498cd136e2c7d556206b3a87774d9b71b
zmhsena <2482811513@qq.com>
2026-02-10 13:40:29 +0800

    Admin update: 2026/2/10 13:39:58

--- data.js
+++ data.js
@@ -1,1 +1,9 @@
-window.blogPosts = [];
+window.blogPosts = [
+  {
+    "title": "我的",
+    "date": "2026-02-10",
+    "excerpt": "确定为![屏幕截图_2026-02-10_192241.png](images/屏幕截图_2026-02-10_192241.png)...",
+    "fullContent": "确定为\n![屏幕截图_2026-02-10_192241.png](images/屏幕截图_2026-02-10_192241.png)",
+    "category": "权威的"
+  }
+];
