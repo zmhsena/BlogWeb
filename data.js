89f3cadf8eb26c8cc6ffd47197f757aa8f2f84a0
zmhsena <2482811513@qq.com>
2026-02-10 13:47:51 +0800

    Admin update: 2026/2/10 13:47:20

--- data.js
+++ data.js
@@ -1,1 +1,9 @@
-window.blogPosts = [];
+window.blogPosts = [
+  {
+    "title": "测试标题",
+    "date": "2026-02-10",
+    "excerpt": "测试代码块    public void InitHeroBlindBox()        {            var btnCoin = transform.Find(\"g-C...",
+    "fullContent": "测试代码块\n```\n    public void InitHeroBlindBox()\n        {\n            var btnCoin = transform.Find(\"g-Coin\").GetComponent<Button>();\n            btnCoin.onClick.RemoveAllListeners();\n            btnCoin.onClick.AddListener(() => { ShowHeroCoinRechargePanel(); });\n        }\n```\n测试图片\n![屏幕截图_2025-12-18_161605.png](images/屏幕截图_2025-12-18_161605.png)\n图片穿插测试\n\n*斜体*\n\n**加粗**\n\n***斜体加粗***\n\n<mark>强调</mark>\n\n<mark>***斜体加粗强调***</mark>\n\n<span style=\"color:#3498db\">改色</span>\n\n测试结束",
+    "category": "测试"
+  }
+];
